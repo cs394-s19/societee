b2c69b7df087219ce346f36449aee64875ae9397
FrancisYL <yiansu66@gmail.com>
2019-05-22 14:55:55 -0500

    change Main

---
+++ components/Main.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+import { Container, Header, Content, Tab, Tabs, TabHeading } from "native-base";
+import EntypoIcon from "react-native-vector-icons/Entypo";
+import Octicon from "react-native-vector-icons/Octicons";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
+import MyMap from './MapPage/MyMap';
+import FriendDisplay from "./ProfilePage/FriendDisplay";
+
+
+export default class Main extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Tabs tabBarPosition="bottom">
+          <Tab
+            heading={
+              <TabHeading>
+                <EntypoIcon name="location" size={30} />
+              </TabHeading>
+            }
+          >
+            <MyMap />
+          </Tab>
+
+          <Tab
+            heading={
+              <TabHeading>
+                <Octicon name="diff-added" size={30} />
+              </TabHeading>
+            }
+          >
+            <View style={styles.container}>
+              <Text style={{ textAlign: "center" }}>Add pins tab</Text>
+            </View>
+          </Tab>
+    
+          <Tab
+            heading={
+              <TabHeading>
+                <FontAwesome name="users" size={30} />
+              </TabHeading>
+            }
+          >
+            <View style={styles.container}>
+              <FriendDisplay />
+            </View>
+          </Tab>
+        </Tabs>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "stretch"
+  }
+});
--- components/MapPage/MyMap.js
+++ components/MapPage/MyMap.js
@@ -5,11 +5,6 @@ import Map from "./Map";
 import { Button, Footer, Text, withTheme } from "react-native-elements";
 import firebase from "../../config/Firebase";
 import "firebase/firestore";
-import { Container, Header, Content, Tab, Tabs, TabHeading } from "native-base";
-import EntypoIcon from "react-native-vector-icons/Entypo";
-import Octicon from "react-native-vector-icons/Octicons";
-import FontAwesome from "react-native-vector-icons/FontAwesome";
-import FriendDisplay from "../ProfilePage/FriendDisplay";
 
 import AddPin from "../Buttons/AddPin";
 
@@ -25,7 +20,7 @@ const db = firebase.firestore();
 var users = db.collection("users");
 var pins = db.collection("pins");
 
-export default class Main extends React.Component {
+export default class MyMap extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -163,7 +158,7 @@ export default class Main extends React.Component {
 
   deletePin = () => {
     //Need PIN ID
-    pins.doc("YEET").delete();
+    pins.doc("D").delete();
   };
 
   addToFavorites = () => {
@@ -218,67 +213,35 @@ export default class Main extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Tabs tabBarPosition="bottom">
-          <Tab
-            heading={
-              <TabHeading>
-                <EntypoIcon name="location" size={30} />
-              </TabHeading>
-            }
-          >
-            <MarkerEdit
-              visible={this.state.markerEdit}
-              closeMarkerEdit={() => this.toggleMarkerEdit()}
-              currEditedPin={this.state.currEditedPin}
-              addPin={pin => this.addPin(pin)}
-            />
-            <SearchBar handlePress={this.handlePress} style={styles.bar} />
-            <Button
-              title="friends pins"
-              onPress={() => this.fetchFriendsPins()}
-            />
-            <Button
-              title="my pins"
-              onPress={() => this.queryPins(this.state.UID)}
-            />
-            <Button
-              title="Edit pin"
-              onPress={() => this.editPin({ hey: "lol" })}
-            />
-            <Button
-              title="Add to favorites"
-              onPress={() => this.addToFavorites()}
-            />
-            <Button title="show modal" onPress={() => this.showMarkerView()} />
-            <Map markers={this.state.markers} />
-            <MarkerView
-              markerPressed={this.state.markerPressed}
-              showMarkerView={this.showMarkerView}
-            />
-          </Tab>
-          <Tab
-            heading={
-              <TabHeading>
-                <Octicon name="diff-added" size={30} />
-              </TabHeading>
-            }
-          >
-            <View style={styles.container}>
-              <Text style={{ textAlign: "center" }}>Add pins tab</Text>
-            </View>
-          </Tab>
-          <Tab
-            heading={
-              <TabHeading>
-                <FontAwesome name="users" size={30} />
-              </TabHeading>
-            }
-          >
-            <View style={styles.container}>
-              <FriendDisplay />
-            </View>
-          </Tab>
-        </Tabs>
+        <MarkerEdit
+          visible={this.state.markerEdit}
+          closeMarkerEdit={() => this.toggleMarkerEdit()}
+          currEditedPin={this.state.currEditedPin}
+          addPin={pin => this.addPin(pin)}
+        />
+        <SearchBar handlePress={this.handlePress} style={styles.bar} />
+        <Button
+          title="friends pins"
+          onPress={() => this.fetchFriendsPins()}
+        />
+        <Button
+          title="my pins"
+          onPress={() => this.queryPins(this.state.UID)}
+        />
+        <Button
+          title="Edit pin"
+          onPress={() => this.editPin({ hey: "lol" })}
+        />
+        <Button
+          title="Add to favorites"
+          onPress={() => this.addToFavorites()}
+        />
+        <Button title="show modal" onPress={() => this.showMarkerView()} />
+        <Map markers={this.state.markers} />
+        <MarkerView
+          markerPressed={this.state.markerPressed}
+          showMarkerView={this.showMarkerView}
+        />
       </View>
     );
   }
