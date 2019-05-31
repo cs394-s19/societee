0f71867b3eeccfa602c7cf33e1eb548ad515f874
FrancisYL <yiansu66@gmail.com>
2019-05-31 14:20:10 -0500

    implement signUpUser and has duplicate check

--- App.js
+++ App.js
@@ -2,27 +2,77 @@ import React from "react";
 import { View, StyleSheet } from "react-native";
 import Login from "./components/LoginPage/Login";
 import Fetcher from "./components/Fetcher";
-import MarkerEdit from "./components/MainPage/MarkerEdit";
+import firebase from "./config/Firebase";
+
+const db = firebase.firestore();
+var users = db.collection("users");
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      user: null
+      user: null,
     };
 
+    this.signUpUser = this.signUpUser.bind(this);
+    this.loginInUser = this.loginInUser.bind(this);
     this.updateUser = this.updateUser.bind(this);
+  }
+
+  signUpUser = name => {
+    var success = false;
+    users.where('name', '==', name)
+         .get()
+         .then(function(querySnapshot) {
+           if (querySnapshot.empty) {
+            success = true;
+           } else {
+             console.log("User: " + name + " already exists");
+           }
+         })
+         .then(() => {
+           if (success) {
+            users.add({
+              name: name,
+              favorites: [],
+              following: []
+            })
+            .then(docRef => {
+              this.setState({ user: docRef.id });
+            });
+           }
+         });
+  }
+
+  loginInUser = name => {
+    
   }
 
   updateUser(uid) {
     this.setState({ user: uid });
   }
 
+  // updateUserType(name) {
+  //   users.add({
+  //     name: name,
+  //     favorites: [],
+  //     following: []
+  //   })
+  //   .then(docRef => {
+  //     this.setState({
+  //       user: docRef.id,
+  //     })
+  //   })
+  //   .catch(error => console.error("Error adding document: ", error))
+  // }
+
   render() {
     return (
       <View style={styles.container}>
         {this.state.user === null ? (
-          <Login updateUser={this.updateUser} />
+          <Login signUpUser={this.signUpUser}
+                 loginInUser={this.loginInUser}
+                 updateUser={this.updateUser} />
         ) : (
           <Fetcher user={this.state.user} />
         )}
