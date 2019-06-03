2015c953b0a4032bed9588d1e5c416b8a9fcf197
Jonathan Dai <jonathandai1226@gmail.com>
2019-06-03 00:58:06 -0500

    we have a homepage now!

--- components/LoginPage/Login.js
+++ components/LoginPage/Login.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { StyleSheet, View, TextInput } from "react-native";
+import {StyleSheet, Keyboard, Text, TouchableOpacity, View, TextInput, Image, TouchableWithoutFeedback, Alert, KeyboardAvoidingView} from 'react-native';
 import { Button, SocialIcon } from "react-native-elements";
 import firebase from "../../config/Firebase";
 
@@ -14,29 +14,61 @@ export default class Login extends Component {
 
   render() {
     return (
-      <View style={styles.container}>
-        <SocialIcon
-          style={{ width: "80%" }}
-          button
-          type="facebook"
-          loading={this.state.loggingin}
-          disabled={this.state.loggingin}
-          onPress={this.loginWithFacebook}
-          title="Log in with Facebook"
-        />
-        
-        <TextInput
-          style={{height: 40, width: 100, borderColor: 'gray', borderWidth: 1}}
-          onChangeText={(name) => this.setState({name})}
-          value={this.state.text}
-        />
-        <Button title="Sign up"
-                onPress={() => this.props.signUpUser(this.state.name)}
-        />
-        <Button title="Log in"
-                onPress={() => this.props.loginInUser(this.state.name)}
-        />
-      </View>
+      <KeyboardAvoidingView style={styles.containerView} behavior="padding">
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={styles.loginScreenContainer}>
+          <View style={styles.loginFormView}>
+            <View style={styles.logo}>
+            <Image source={require('../societee_logo.png')}  style={{width: 300, height: 300}} />
+            </View>
+            <TextInput placeholder="Username" placeholderColor="#c4c3cb" style={styles.loginFormTextInput} />
+            <Button
+              buttonStyle={styles.loginButton}
+              onPress={() => this.props.loginInUser(this.state.name)}
+              title="Login"
+              color= "#FDEBE1"
+            />
+            <Button
+              buttonStyle={styles.loginButton}
+              onPress={() => this.props.signUpUser(this.state.name)}
+              title="Signup"
+              color= "#FDEBE1"
+            />
+            <TouchableOpacity 
+               style={styles.fbLoginButton}
+               onPress={this.loginWithFacebook}>
+              <View>
+              <Text style={styles.fbLoginText}>Login with Facebook</Text>
+              </View>
+             </TouchableOpacity>
+          </View>
+        </View>
+      </TouchableWithoutFeedback>
+      </KeyboardAvoidingView>
+
+      
+      // <View style={styles.container}>
+      //   <TextInput
+      //     style={{height: 40, width: 100, borderColor: 'gray', borderWidth: 1}}
+      //     onChangeText={(name) => this.setState({name})}
+      //     value={this.state.text}
+      //   />
+      //   <Button title="Sign up"
+      //           onPress={() => this.props.signUpUser(this.state.name)}
+      //   />
+      //   <Button title="Log in"
+      //           onPress={() => this.props.loginInUser(this.state.name)}
+      //   />
+      //    <SocialIcon
+      //     style={{ width: "80%" }}
+      //     button
+      //     type="facebook"
+      //     loading={this.state.loggingin}
+      //     disabled={this.state.loggingin}
+      //     onPress={this.loginWithFacebook}
+      //     title="Login with Facebook"
+      //   />
+      // </View>
     );
   }
 
@@ -82,9 +114,57 @@ export default class Login extends Component {
 }
 
 const styles = StyleSheet.create({
-  container: {
+  containerView: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center"
+  },
+  loginScreenContainer: {
+    flex: 1,
+  },
+  logo:{
+    marginTop: 60,
+    alignItems: 'center'
+  },
+  logoText: {
+    fontSize: 40,
+    fontWeight: "800",
+    marginTop: 150,
+    marginBottom: 30,
+    textAlign: 'center',
+  },
+  loginFormView: {
+    flex: 1
+  },
+  loginFormTextInput: {
+    height: 43,
+    fontSize: 14,
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: '#eaeaea',
+    backgroundColor: '#fafafa',
+    paddingLeft: 10,
+    marginLeft: 15,
+    marginRight: 15,
+    marginTop: 5,
+    marginBottom: 5,
+  
+  },
+  loginButton: {
+    backgroundColor: '#E64A4B',
+    borderRadius: 5,
+    height: 45,
+    marginLeft: 15,
+    marginRight: 15,
+    justifyContent: 'center',
+    alignItems: 'center',  
+    marginTop: 10,
+  },
+  fbLoginButton: {
+    height: 45,
+    justifyContent: 'center',
+    alignItems: 'center', 
+  },
+  fbLoginText:{
+    color: '#3897f1',
+    marginTop: 17, 
   }
 });
